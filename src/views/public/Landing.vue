05a8123e7de501618435f41b171746f1994a880a
Juan Pablo Vallejo <jpvallejo@columbus.edu.co>
2022-10-06 10:51:38 -0500

    nuevo inicio

--- src/views/public/Landing.vue
+++ src/views/public/Landing.vue
@@ -1,8 +1,33 @@
 <template>
     <q-toolbar class="text-primary">
       <q-toolbar-title>
-        Toolbar
+        Webstuhl
       </q-toolbar-title>
     </q-toolbar>
-    <h1>Landing</h1>
+
+    <div class="q-pa-md row items-start q-gutter-md">
+      <q-card class="my-card" flat bordered>
+        <q-card-section horizontal>
+          <q-card-section class="q-pt-xs">
+            <div class="text-overline">Vacante</div>
+            <div class="text-h5 q-mt-sm q-mb-xs">Titulo</div>
+            <div class="text-caption text-grey">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </div>
+          </q-card-section>
+        </q-card-section>
+
+        <q-separator />
+
+        <q-card-actions>
+          <q-btn flat round icon="event" />
+          <q-btn flat>
+            7:30PM
+          </q-btn>
+          <q-btn flat color="primary">
+            Reserve
+          </q-btn>
+        </q-card-actions>
+      </q-card>
+    </div>
 </template>
